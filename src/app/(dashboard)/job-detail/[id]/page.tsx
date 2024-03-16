7ef6db1f7a80c40fdadaa2fae9e68f2b3ce8302c
ramadhan8g <ramadhanyandra13@gmail.com>
2024-03-16 17:03:15 +0700

    first commit

---
+++ src/app/(dashboard)/job-detail/[id]/page.tsx
@@ -0,0 +1,76 @@
+import { ArrowLeftIcon } from "lucide-react";
+import Link from "next/link";
+import { FC } from "react";
+
+import Applicants from "@/components/organisms/Applicants";
+import JobDetail from "@/components/organisms/JobDetail";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import prisma from "../../../../../lib/prisma";
+
+type paramsType = {
+	id: string;
+};
+interface JobDetailPageProps {
+	params: paramsType;
+}
+
+export const revalidate = 0;
+
+async function getDetailJob(id: string) {
+	const job = await prisma.job.findFirst({
+		where: {
+			id: id,
+		},
+		// panggil relasi
+		include: {
+			applicant: {
+				include: {
+					user: true,
+				},
+			},
+			CategoryJob: true,
+		},
+	});
+
+	return job;
+}
+
+const JobDetailPage: FC<JobDetailPageProps> = async ({ params }) => {
+	const job = await getDetailJob(params.id);
+
+	return (
+		<div>
+			<div className="inline-flex items-center gap-5 mb-5">
+				<div>
+					<Link href="/job-listings">
+						<ArrowLeftIcon className="w-9 h-9" />
+					</Link>
+				</div>
+				<div>
+					<div className="text-2xl font-semibold mb-1">
+						{job?.roles}
+					</div>
+					<div>
+						{job?.CategoryJob?.name} . {job?.jobType} .{" "}
+						{job?.applicants}/{job?.needs} Hired
+					</div>
+				</div>
+			</div>
+
+			<Tabs defaultValue="applicants">
+				<TabsList className="mb-8">
+					<TabsTrigger value="applicants">Applicants</TabsTrigger>
+					<TabsTrigger value="jobDetails">Job Details</TabsTrigger>
+				</TabsList>
+				<TabsContent value="applicants">
+					<Applicants applicants={job?.applicant} />
+				</TabsContent>
+				<TabsContent value="jobDetails">
+					<JobDetail detail={job} />
+				</TabsContent>
+			</Tabs>
+		</div>
+	);
+};
+
+export default JobDetailPage;
---
+++ src/app/(dashboard)/job-listings/page.tsx
@@ -0,0 +1,103 @@
+import { FC } from "react";
+
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import ButtonActionTable from "@/components/organisms/ButtonActionTable";
+import { Badge } from "@/components/ui/badge";
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "@/components/ui/table";
+import { JOB_LISTING_COLUMNS } from "@/constants";
+import { dateFormat } from "@/lib/utils";
+import { Job } from "@prisma/client";
+import moment from "moment";
+import { getServerSession } from "next-auth";
+import prisma from "../../../../lib/prisma";
+
+interface JobListingsPageProps {}
+
+export const revalidate = 0;
+
+async function getDataJobs() {
+	const session = await getServerSession(authOptions);
+	// panggil prisma, ambil data berdasarkan session yg login
+	const jobs = prisma.job.findMany({
+		where: {
+			companyId: session?.user.id,
+		},
+	});
+
+	return jobs;
+}
+
+const JobListingsPage: FC<JobListingsPageProps> = async ({}) => {
+	const jobs = await getDataJobs();
+
+	console.log(jobs);
+
+	return (
+		<div>
+			<div className="font-semibold text-3xl">Job Listings</div>
+
+			<div className="mt-10">
+				<Table>
+					<TableHeader>
+						<TableRow>
+							{JOB_LISTING_COLUMNS.map(
+								(item: string, i: number) => (
+									<TableHead key={item + i}>{item}</TableHead>
+								)
+							)}
+							<TableHead>Action</TableHead>
+						</TableRow>
+					</TableHeader>
+					<TableBody>
+						{jobs.map((item: Job, i: number) => (
+							<TableRow key={item.roles + i}>
+								<TableCell>{item.roles}</TableCell>
+								<TableCell>
+									{/* cek misalkan tgl posting ini sebelum dataline jobnya d tutup maka status nya live */}
+									{moment(item.datePosted).isBefore(
+										item.dueDate
+									) ? (
+										<Badge>Live</Badge>
+									) : (
+										<Badge variant="destructive">
+											Expired
+										</Badge>
+									)}
+								</TableCell>
+								<TableCell>
+									{dateFormat(item.datePosted)}
+								</TableCell>
+								<TableCell>
+									{dateFormat(item.dueDate)}
+								</TableCell>
+								<TableCell>
+									<Badge variant="outline">
+										{item.jobType}
+									</Badge>
+								</TableCell>
+								<TableCell>{item.applicants}</TableCell>
+								<TableCell>
+									{item.applicants} / {item.needs}
+								</TableCell>
+								<TableCell>
+									<ButtonActionTable
+										url={`/job-detail/${item.id}`}
+									/>
+								</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			</div>
+		</div>
+	);
+};
+
+export default JobListingsPage;
